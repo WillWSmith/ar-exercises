3be44aa224735a001c43b21e1ee545909b40bb8d
WillWSmith <will.w.smith94@gmail.com>
2024-05-13 16:22:32 -0400

    Add ex5 queries

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+
+@average_revenue = Store.average(:annual_revenue)
+
+@stores_over_million = Store.where("annual_revenue > 1000000").count
+
+puts "Total revenue: #{@total_revenue}"
+
+puts "Average revenue: #{@average_revenue}"
+
+puts "Number of stores with revenue over 1 million: #{@stores_over_million}"
